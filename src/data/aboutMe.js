a50049b2ae6bc13cb5b4f91eb67935010e1591e5
Aadish Rathore <adib.rathore@gmail.com>
2025-10-08 17:30:37 -0700

    initial commit

--- src/data/aboutMe.js
+++ src/data/aboutMe.js
@@ -1,4 +1,8 @@
 
 // About me
-export const aboutMe = `
-I'm a Machine Learning Engineer with 6+ years of production software experience and a Master’s in Computer Science (Data Science). I specialize in NLP, Transformers, and deploying ML pipelines using PyTorch, Hugging Face, and AWS. My projects include a house price predictor (R² 0.88) and a sentiment analysis platform with TF-IDF+SVM, LSTM, and DistilBERT. I’ve built modular backends with NestJS, responsive React frontends, and RESTful APIs for real-time inference. At TD Bank, I developed scalable financial apps and reusable UI components in Agile teams. I thrive at the intersection of ML, software architecture, and user-centric design—always pushing for impact, polish, and innovation across the stack.`;
+export const aboutMe = `I'm a Machine Learning Engineer with 6+ years of production software experience and a Master’s in Computer Science (Data Science).
+I specialize in NLP, Transformers, and deploying ML pipelines using PyTorch, Hugging Face, and AWS. My projects include a house price predictor (R² 0.88) and a
+sentiment analysis platform with TF-IDF+SVM, LSTM, and DistilBERT. I’ve built modular backends with NestJS, responsive React frontends, and RESTful APIs for real-time
+inference. At TD Bank, I developed scalable financial apps and reusable UI components in Agile teams. I thrive at the intersection of ML, software architecture, and
+user-centric design—always pushing for impact, polish, and innovation across the stack.
+`;
